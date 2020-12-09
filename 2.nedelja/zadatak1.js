87367fb98d4d5ba9fe7951236263ea96d939ce43
nikolamandicarh <nikola.mandic.arh@gmail.com>
2020-12-09 17:08:01 +0100

    Finished 1

---
+++ 2.nedelja/zadatak1.js
@@ -0,0 +1,17 @@
+{
+    let a = 5
+    b = 8
+
+    if (a<b){
+        temp = a
+        a = b
+        b = temp
+    }
+    sum = a + b
+    sub = a - b
+    mul = a * b
+    div = a / b
+    res = a % b
+    squ = a ** b
+}
+console.log (sum, sub, mul, div, res, squ)
