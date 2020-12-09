1620f5d31eed48bcfaa2658af6cf1e6088ad530b
nikolamandicarh <nikola.mandic.arh@gmail.com>
2020-12-09 17:30:07 +0100

    Finished 3

---
+++ 2.nedelja/zadatak3.js
@@ -0,0 +1,12 @@
+{
+    let a = 13
+
+    if (a % 3 == 0) {
+        poruka = "a_je deljiv sa 3"
+        console.log(poruka)
+    }
+    else {
+        poruka = "a_nije deljiv sa 3"
+        console.log(poruka)
+    }
+}
